f3ad9c08901814461b03bd65209ac83856bfffb9
Damianp99 <damianperez200199@gmail.com>
2022-02-24 16:10:37 +0100

    added deleted word

--- index.php
+++ index.php
@@ -9,6 +9,12 @@ Stampare di nuovo il paragrafo e la sua lunghezza, dopo aver sostituito con tre 
 $paragraph = "Lorem ipsum dolor sit amet consectetur adipisicing elit. Nostrum laborum amet nemo, repellendus consequatur inventore aut cupiditate sapiente repudiandae veniam tempora maiores, voluptates velit tempore ipsum officia pariatur, illo cumque.";
 
 
+$word = $_GET['word'];
+
+$deleted = "***";
+
+
+
 ?>
 
 <!DOCTYPE html>
@@ -17,15 +23,27 @@ $paragraph = "Lorem ipsum dolor sit amet consectetur adipisicing elit. Nostrum l
     <meta charset="UTF-8">
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <link rel="stylesheet" href="css/style.css">
     <title>Document</title>
 </head>
 <body>
     <main>
+        <!-- PARAGRAFO INTEGRALE -->
         <div>
-           
             <p><?php  echo $paragraph?>  <span><?php echo strlen($paragraph) ?></span></p>
         </div>
+        <!-- FORM TRAMITE CUI INVIAMO LA PAROLA DA CENSURARE -->
+        <div>
+            <form action="index.php" method="GET">
+                <input type="text" name="word" placeholder="Inserisci una parola da censurare">
+                <button type="submit">Invia</button>
+
+            </form>
+        </div>
+        <!-- NUOVO PARAGRAFO CON LA PAROLA CENSURATA -->
+        <div>
+            <p><?php  echo str_replace($word,$deleted,$paragraph)?>  <span><?php echo strlen($paragraph) ?></span></p>
+        </div>
+        
     </main>
 </body>
 </html>
